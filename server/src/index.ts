ef5a21a512ba123b7862d89c13733877527fefd3
EmirYscn <87046103+EmirYscn@users.noreply.github.com>
2025-02-13 22:22:46 +0300

    cors fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,11 @@ import { router as authRouter } from "./routes/authRoutes";
 dotenv.config({ path: "./.env" });
 
 const app = express();
-const allowedOrigins = [process.env.CLIENT_URL!, "http://localhost:5173"];
+const allowedOrigins = [
+  process.env.CLIENT_URL!,
+  "http://localhost:5173",
+  "https://file-uploader-azure.vercel.app/",
+];
 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
